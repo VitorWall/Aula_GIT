e109ba359d54de30586e8f79312d1f7e6e1cbf00
mac0321 21 <vitor.wallace@hotmail.com>
2022-06-02 11:29:00 -0300

    acrescentando opção de segunda linguagem

---
+++ lerolero.py
@@ -0,0 +1,16 @@
+"""Gerador de frases de efeito aleatórias"""
+
+import random
+
+parte1 = []
+parte2 = []
+parte3 = []
+
+linguagem = 1
+
+if == 2
+    parte1 = []
+    parte2 = []
+    parte3 = []
+
+print (random.choice(parte1), random.choice(parte2), random.choice(parte3))
